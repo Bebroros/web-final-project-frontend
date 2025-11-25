48c4b55098deb9b48970325932a0e895ff94aebb
kvstkk <katyav120064@gmail.com>
2025-11-25 13:34:32 +0200

    first  setup check

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+const LoginPage = () => {
+    return (
+        <Container>
+            <Typography variant="h4">Login page</Typography>
+        </Container>
+    );
+};
+export default LoginPage;
